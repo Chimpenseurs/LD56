e3fb7a1b0184ce3f63260a54dbe2b9b8fce3477f
jriffet <jeremy.riffet@gmail.com>
2024-10-06 17:32:55 +0200

    add creature_4

--- godot/scripts/scene.gd
+++ godot/scripts/scene.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
-var speed_camera = 8.0
+var speed_camera = 20.0
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_RIGHT or event.keycode == KEY_LEFT:
@@ -23,7 +23,8 @@ func _input(event: InputEvent) -> void:
 				
 		if event.keycode == KEY_RIGHT:
 			$Camera2D.position.x += speed_camera
-			$Camera2D.position.x = min($Camera2D.position.x, 632.0)
+			#$Camera2D.position.x = min($Camera2D.position.x, 632.0)
+			$Camera2D.position.x = min($Camera2D.position.x, 5632.0)
 		elif event.keycode == KEY_LEFT:
 			$Camera2D.position.x -= speed_camera
 			$Camera2D.position.x = max($Camera2D.position.x, 0.0)
